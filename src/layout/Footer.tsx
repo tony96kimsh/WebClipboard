32daf5dfdbb1595b85516021fc46af4160eedf19
Tony <tony96kimsh@gmail.com>
2025-05-28 09:31:03 +0900

    feat: update Footer component with GitHub link and improve styling

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -3,10 +3,23 @@ import { Container } from "react-bootstrap";
 const Footer : React.FC = () =>{
     return(
         <footer className="text-white py-1 mt-5" style = {{backgroundColor : '#4169E1'}}>
-            <Container className = "text-center">
-                대표 : 김성훈<br/>
-                이메일 : <a href = "mailto:tony96kimsh@naver.com" className="text-white">tony96kimsh@naver.com</a><br/>
-                Copyright 김성훈 Corp. All rights Reserved.<br/>
+            <Container className = "text-light " >
+              <br />
+              <p className="m-0">
+                <a href="https://github.com/tony96kimsh/WebClipboard" 
+                  style={{color:'#ddd',}}
+                  className="text-decoration-none"
+                >
+                  깃허브 링크 : https://github.com/tony96kimsh/WebClipboard
+                </a>
+              </p>
+                <p style={{color:'#ddd',}} className="m-0">
+                  이메일 : <a href = "mailto:tony96kimsh@naver.com" className="text-white text-decoration-none" style={{color:'#ddd'}}>tony96kimsh@naver.com</a>
+                </p>
+                <p style={{color:'#ddd',}}>
+                  Copyright 김성훈 Corp. All rights Reserved.
+                </p>
+                <br />
             </Container>
         </footer>
     )
